0d87233d423bcf2a0f20ee53d80f18e7b2c76b6b
Fernando Suares <fernandoaqs@outlook.com.br>
2024-10-09 09:00:38 -0300

    refactor(dependencies): update express and celebrate packages in app

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express from 'express'
+import 'reflect-metadata'
+import 'express-async-errors'
 import cors from 'cors'
+import { errors } from 'celebrate'
+import express from 'express'
 
 export const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.use(errors())
