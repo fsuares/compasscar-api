56919976f1d7462a7adc8d8aab8f08766d4c75d5
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-09 14:03:00 -0300

    feat(server.ts): add Customers Router on server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,14 @@ import 'dotenv/config'
 import { app } from './app'
 import './database/data-source'
 
+import { Router } from 'express'
+import customersRouter from './models/customers/routes/CustomersRoutes'
+
 const port = process.env.PORT || 3003
+
+const routes = Router()
+app.use(routes)
+routes.use('/customers', customersRouter)
 
 const server = app.listen(port, () => {
   console.log(`App started on port ${port}`)
