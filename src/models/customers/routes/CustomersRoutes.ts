984df01d6cafe97e91984c945e371f5de44ade1b
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-09 21:25:57 -0300

    feat(customers): creates the customer POST route with all validations with celebrate

--- src/models/customers/routes/CustomersRoutes.ts
+++ src/models/customers/routes/CustomersRoutes.ts
@@ -1,9 +1,35 @@
 import { Router } from 'express'
+import { celebrate, Joi, Segments } from 'celebrate'
+import isValidCpf from 'middlewares/isValidCpf'
 import CustomersController from '../../../controllers/CustomersController'
 
 const customersRouter = Router()
 const customersController = new CustomersController()
 
-customersRouter.post('/', customersController.create)
+customersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      birth_date: Joi.date().iso().min('1910-01-01').max('now').required(),
+      cpf: Joi.string()
+        .pattern(/^\d{9}-\d{2}$/)
+        .required()
+        .messages({
+          'string.pattern.base': 'The CPF must be in the format 123123123-99.'
+        }),
+      email: Joi.string().email().required(),
+      phone: Joi.string()
+        .pattern(/^[0-9()+\-\s]+$/)
+        .required()
+        .messages({
+          'string.pattern.base':
+            '"phone" must contain only numbers and allowed characters (+, -, (), space).'
+        })
+    }
+  }),
+  isValidCpf,
+  customersController.create
+)
 
 export default customersRouter
