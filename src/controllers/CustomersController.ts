5d233066fb1402a3583606ec9e347c2dc8981e49
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-10 16:13:06 -0300

    feat(customers): implement the service of deleting a customer in the customers controller

--- src/controllers/CustomersController.ts
+++ src/controllers/CustomersController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { CreateCustomerService } from '@customers/services/CreateCustomerService'
 import { ShowCustomerService } from '@customers/services/ShowCustomerService'
+import { DeleteCustomerService } from '@customers/services/DeleteCustomerService'
 
 export default class CustomersController {
   public async create(req: Request, res: Response): Promise<any> {
@@ -29,5 +30,12 @@ export default class CustomersController {
     })
 
     return res.status(200).json(customer)
+  }
+
+  public async delete(req: Request, res: Response): Promise<any> {
+    const { id } = req.params
+
+    const deleteCustomer = await new DeleteCustomerService().execute(id)
+    return res.status(204).send()
   }
 }
