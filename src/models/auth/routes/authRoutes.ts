42d39291e1bfa3ef0f57a16f3fe185152a7a341f
Rogerio Cordeiro <56833456+RogerioCordeiro@users.noreply.github.com>
2024-10-11 11:24:43 -0300

    Update authRoutes.ts

--- src/models/auth/routes/authRoutes.ts
+++ src/models/auth/routes/authRoutes.ts
@@ -11,7 +11,7 @@ authRouter.post(
   celebrate({
     [Segments.BODY]: Joi.object().keys({
       email: Joi.string().email().required(),
-      password: Joi.string().min(6).required()
+      password: Joi.string().min(8).required()
     })
   }),
   (req, res) => authController.login(req, res)
