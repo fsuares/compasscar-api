1547c8e9b8ff41c1d62ca6da6bf79833b3a1c221
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-11 17:59:36 -0300

    refactor(customers): changes date of birth validation and implements patch customer route

--- src/models/customers/routes/CustomersRoutes.ts
+++ src/models/customers/routes/CustomersRoutes.ts
@@ -13,12 +13,23 @@ customersRouter.post(
   celebrate({
     [Segments.BODY]: {
       name: Joi.string().required(),
-      birth_date: Joi.date().iso().min('1910-01-01').max('now').required(),
+      birth_date: Joi.custom((value, helpers) => {
+        const date = new Date(value)
+        const [year, month, day] = value.substring(0, 10).split('-').map(Number)
+        if (
+          date.getFullYear() !== year ||
+          date.getMonth() + 1 !== month ||
+          date.getDate() + 1 !== day
+        ) {
+          return helpers.error('any.invalid')
+        }
+        return value
+      }).required(),
       cpf: Joi.string()
         .pattern(/^\d{9}-\d{2}$/)
         .required()
         .messages({
-          'string.pattern.base': 'The CPF must be in the format 123123123-99.'
+          'string.pattern.base': 'the CPF must be in the format 123123123-99.'
         }),
       email: Joi.string().email().required(),
       phone: Joi.string()
@@ -52,6 +63,44 @@ customersRouter.delete(
     }
   }),
   customersController.delete
+)
+
+customersRouter.patch(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required()
+    },
+    [Segments.BODY]: {
+      name: Joi.string(),
+      birth_date: Joi.custom((value, helpers) => {
+        const date = new Date(value)
+        const [year, month, day] = value.substring(0, 10).split('-').map(Number)
+        if (
+          date.getFullYear() !== year ||
+          date.getMonth() + 1 !== month ||
+          date.getDate() + 1 !== day
+        ) {
+          return helpers.error('any.invalid')
+        }
+        return value
+      }),
+      cpf: Joi.string()
+        .pattern(/^\d{9}-\d{2}$/)
+        .messages({
+          'string.pattern.base': 'the CPF must be in the format 123123123-99.'
+        }),
+      email: Joi.string().email(),
+      phone: Joi.string()
+        .pattern(/^[0-9()+\-\s]+$/)
+        .messages({
+          'string.pattern.base':
+            '"phone" must contain only numbers and allowed characters (+, -, (), space).'
+        })
+    }
+  }),
+  isValidCpf,
+  customersController.update
 )
 
 export default customersRouter
