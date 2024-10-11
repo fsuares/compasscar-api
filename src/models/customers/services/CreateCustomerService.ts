4e75d80cee8db4ddaaf9ffdb68634925365b1298
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-11 17:58:26 -0300

    refactor(customers): changes the birth_date type and statuses for conflicts in requests

--- src/models/customers/services/CreateCustomerService.ts
+++ src/models/customers/services/CreateCustomerService.ts
@@ -22,19 +22,22 @@ export class CreateCustomerService {
       await CustomersRepository.findByEmailAndCheckIfExcludedIsNull(email)
 
     if (ifEmailExists) {
-      throw new AppError('Email already exists and the customer is active.')
+      throw new AppError(
+        'Email already exists and the customer is active.',
+        409
+      )
     }
 
     const ifCpfExists =
       await CustomersRepository.findByCpfAndCheckIfExcludedIsNull(cpf)
 
     if (ifCpfExists) {
-      throw new AppError('Cpf already exists and the customer is active.')
+      throw new AppError('Cpf already exists and the customer is active.', 409)
     }
 
     const customer = CustomersRepository.create({
       name,
-      birth_date,
+      birth_date: birth_date.substring(0, 10),
       cpf,
       email,
       phone
