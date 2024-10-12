6c0889e75a0d468c8e200b2ef2ca5a2f905bd4df
Fernando Suares <fernandoaqs@outlook.com.br>
2024-10-12 05:03:33 -0300

    refactor(cars): improve delete and update car services

--- src/modules/cars/services/DeleteCarService.ts
+++ src/modules/cars/services/DeleteCarService.ts
@@ -1,12 +1,21 @@
 import AppError from '@errors/AppError'
 import { CarsRepository } from '@cars/repositories/CarsRepository'
+import { OrdersRepository } from '@orders/repositories/OrdersRepository'
 
 export class DeleteCarService {
   public async execute(id: string): Promise<void> {
     const car = await CarsRepository.findByID(id)
-    if (!car) {
-      throw new AppError('Car not found', 404)
+    if (!car || car.status === 'excluido') {
+      throw new AppError('Car not found or excluded', 404)
     }
+
+    const carOrder = await OrdersRepository.findByCar(id)
+    carOrder.forEach((order) => {
+      if (order.status === 'open' || order.status === 'approved') {
+        throw new AppError(`Can't delete... Car currently rented`, 409)
+      }
+    })
+
     await CarsRepository.deleteCar(id)
   }
 }
--- src/modules/cars/services/UpdateCarService.ts
+++ src/modules/cars/services/UpdateCarService.ts
@@ -1,7 +1,7 @@
 import AppError from '@errors/AppError'
 import { CarsRepository } from '@cars/repositories/CarsRepository'
 interface IRequest {
-  id: string;
+  id: string
   license_plate: string
   brand: string
   model: string
@@ -24,21 +24,19 @@ export class UpdateCarService {
     status
   }: IRequest): Promise<any> {
     const car = await CarsRepository.findByID(id)
-    if (!car) {
+    if (!car || car.status === 'excluido') {
       throw new AppError('Car not found', 404)
     }
-    const carAlreadyExists = await CarsRepository.findByPlate(license_plate)
-    if(car.status === 'excluido'){
-      throw new AppError('Car not found', 404)
-    }
-    car.license_plate = license_plate;
-    car.brand = brand;
-    car.model = model;
-    car.km = km;
-    car.year = year;
-    car.price = price;
-    car.items = items;
-    car.status = status;
+
+    car.license_plate = license_plate
+    car.brand = brand
+    car.model = model
+    car.km = km
+    car.year = year
+    car.price = price
+    car.items = items
+    car.status = status
+
     await CarsRepository.save(car)
     return car
   }
