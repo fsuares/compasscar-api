c838a37aac9448d7028e46a800db3aed0c7b7588
Fernando Suares <fernandoaqs@outlook.com.br>
2024-10-12 11:30:35 -0300

    feat(orders): add show and delete methods to OrdersController

--- src/modules/orders/controllers/OrdersController.ts
+++ src/modules/orders/controllers/OrdersController.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from 'express'
 import { CreateOrderService } from '@orders/services/CreateOrderService'
 import { UpdateOrderService } from '@orders/services/UpdateOrderService'
-import { ShowOrderService } from '@orders/services/ShowOrderService'
-import { DeleteOrderService } from '@orders/services/DeleteOrderService'
 import { FindByIdOrderService } from '@orders/services/FindbyOrderService'
 import { IOrderResponse } from '@orders/interfaces/OrdersInterface'
+import { ShowOrderService } from '@orders/services/ShowOrderService'
+import { DeleteOrderService } from '@orders/services/DeleteOrderService'
 
 export default class OrdersController {
   public async create(req: Request, res: Response): Promise<string | any> {
@@ -36,7 +36,7 @@ export default class OrdersController {
     return res.status(200).json(order)
   }
 
-  public async update(req: Request, res: Response): Promise<string | any> {
+  public async update(req: Request, res: Response): Promise<any> {
     const { id } = req.params
     const { start_date, end_date, cep, status } = req.body
 
@@ -53,12 +53,6 @@ export default class OrdersController {
     return res.status(200).json(order)
   }
 
-  public async delete(req: Request, res: Response): Promise<any> {
-    const { id } = req.params
-    await new DeleteOrderService().execute(id)
-    return res.status(204).json()
-  }
-
   public async findById(
     req: Request,
     res: Response
@@ -67,5 +61,11 @@ export default class OrdersController {
     const findByIdOrderService = new FindByIdOrderService()
     const order = await findByIdOrderService.execute(id)
     return res.status(201).json(order)
+  }
+
+  public async delete(req: Request, res: Response): Promise<any> {
+    const { id } = req.params
+    await new DeleteOrderService().execute(id)
+    return res.status(204).json()
   }
 }
