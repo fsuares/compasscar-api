02c3dd0c56f99ee28345775f7595fa9e12a92d31
Guilherme Dilio de Souza <thekingguiiz2003@gmail.com>
2024-10-13 15:14:25 -0300

    style(customers): changing the writing pattern of validation error messages to lowercase letters

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -1,14 +1,6 @@
-import { Customer } from '@customers/entities/Customer'
+import { ICreateCustomer } from '@customers/interfaces/CustomersInterfaces'
 import { CustomersRepository } from '@customers/repositories/CustomersRepository'
 import AppError from '@errors/AppError'
-
-interface ICreateCustomer {
-  name: string
-  birth_date: string
-  cpf: string
-  email: string
-  phone: string
-}
 
 export class CreateCustomerService {
   public async execute({
@@ -17,13 +9,13 @@ export class CreateCustomerService {
     cpf,
     email,
     phone
-  }: ICreateCustomer): Promise<Customer> {
+  }: ICreateCustomer): Promise<String> {
     const ifEmailExists =
       await CustomersRepository.findByEmailAndCheckIfExcludedIsNull(email)
 
     if (ifEmailExists) {
       throw new AppError(
-        'Email already exists and the customer is active.',
+        'email already exists and the customer is active.',
         409
       )
     }
@@ -32,18 +24,18 @@ export class CreateCustomerService {
       await CustomersRepository.findByCpfAndCheckIfExcludedIsNull(cpf)
 
     if (ifCpfExists) {
-      throw new AppError('Cpf already exists and the customer is active.', 409)
+      throw new AppError('cpf already exists and the customer is active.', 409)
     }
 
     const customer = CustomersRepository.create({
       name,
-      birth_date: birth_date.substring(0, 10),
+      birth_date: birth_date?.substring(0, 10),
       cpf,
       email,
       phone
     })
 
     await CustomersRepository.save(customer)
-    return customer
+    return customer.id
   }
 }
